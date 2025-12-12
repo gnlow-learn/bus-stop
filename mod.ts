cd078f2fc9ce930b93d25bac7dcd22edfec8be7c
Gnlow <gnlow@pm.me>
2025-12-12 17:18:21 +0900

    refactor

--- mod.ts
+++ mod.ts
@@ -1,29 +1,3 @@
-import raw from "./orig/국토교통부_전국 버스정류장 위치정보_20251031.csv" with { type: "bytes" }
+import { data } from "./src/data.ts"
 
-const decode =
-(raw: Uint8Array<ArrayBuffer>) =>
-    new TextDecoder("euc-kr").decode(raw)
-
-const split =
-(str: string) =>
-    str
-        .split("\r\n")
-        .map(x => x.split(","))
-
-const zip =
-<T extends unknown[]>(...ass: T[]) =>
-    ass[0].map((_, i) => ass.map(x => x[i]))
-
-const wrap =
-(data: string[][]) => {
-    const [head, ...body] = data
-    return body.map(v =>
-        Object.fromEntries(
-            zip(head, v)
-        )
-    )
-}
-
-console.log(
-    wrap(split(decode(raw)))[0]
-)
+console.log(data[0])
